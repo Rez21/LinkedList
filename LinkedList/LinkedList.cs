37c9cf5d4989681ff6a1f132c392b5db6b4e15b6
Prasad <banprasad21@gmail.com>
2022-09-08 00:58:40 +0530

    [Raghav] Add: created simple linked list of 56 ,30 and 70

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tempLastNode;
+        public void InsertAtLast(T data)
+        {
+            // If the linked list is empty
+            if (this.head == null)
+            {
+                this.head = new Node<T>(data);
+                tempLastNode = head;
+                return;
+            }
+
+            // If the head is already full
+            tempLastNode.next = new Node<T>(data);
+            tempLastNode = tempLastNode.next;
+        }
+        public void DisplayLinkedList()
+        {
+            // If linked list is empty
+            if (this.head == null)
+            {
+                Console.WriteLine("No elements in linked list");
+                return;
+            }
+            // If linked list has elements then iterate till the next of a node is null
+            Node<T> tempNode = this.head;
+            Console.WriteLine("The elements are : ");
+            while (tempNode != null)
+            {
+                Console.Write(tempNode.data + "\t");
+                tempNode = tempNode.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List!");
+
+            // Initialising a linked list
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            // Adding elements to linkedlist
+            linkedList.InsertAtLast(56);
+            linkedList.InsertAtLast(30);
+            linkedList.InsertAtLast(70);
+
+            // Displaying linked list
+            linkedList.DisplayLinkedList();
+        }
+    }
+}
